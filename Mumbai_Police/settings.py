6a4573dea968d6a833c3dde32d5a65c2393b0606
Divesh2201 <diveshthakker2201@gmail.com>
2021-02-07 00:17:23 +0530

    minor changes

--- Mumbai_Police/settings.py
+++ Mumbai_Police/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'home.apps.HomeConfig',
+    'complaints.apps.ComplaintsConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
     'django.contrib.admin',
--- Mumbai_Police/urls.py
+++ Mumbai_Police/urls.py
@@ -24,4 +24,6 @@ urlpatterns = [
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('complaints/', include('complaints.urls')),
+    path('', include('home.urls'))
 ]
