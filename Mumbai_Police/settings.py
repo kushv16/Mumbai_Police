9816925dbe4601fcf715e9ba144ce99e2a066152
Divesh2201 <diveshthakker2201@gmail.com>
2021-02-06 23:26:28 +0530

    database integration

--- Mumbai_Police/settings.py
+++ Mumbai_Police/settings.py
@@ -80,8 +80,12 @@ WSGI_APPLICATION = 'Mumbai_Police.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'Mumbai_Police',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
 }
 
