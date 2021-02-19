6e9002d003bad0ac023ff91ede56686c7a4eb348
Divesh2201 <diveshthakker2201@gmail.com>
2021-02-19 11:47:57 +0530

    maha-data migration

--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from geo import load_layer
 
 # Create your views here.
 from django.http import HttpResponse
 
 def home(request):
+    # load_layer.run()
     return render(request, 'home/home.html')
