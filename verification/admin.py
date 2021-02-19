ad8019617481d7b2a13060977da03f5ec79eec58
Kush Vora <vorakush@gmail.com>
2021-02-19 22:58:59 +0530

    admin ui

--- verification/admin.py
+++ verification/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
-
+from .models import VerificationInfo
 # Register your models here.
+
+
+class VerificationInfoAdmin(admin.ModelAdmin):
+    readonly_fields = ('propOwnerFullName',
+                       'propOwnerContact',
+                       'propOwnerEmail',
+                       'propOwnerFlatno',
+                       'propOwnerAddress',
+                       'propOwnerCity',
+                       'propOwnerState',
+                       'propOwnerCountry',
+                       'tenantFullName',
+                       'tenantContact',
+                       'tenantEmail',
+                       'tenantFlatno',
+                       'tenantAddress',
+                       'tenantCity',
+                       'tenantState',
+                       'tenantCountry',
+                       'tenantDOB',
+                       'tenantReason',
+                       'tenantOrganisation',
+                       'tenantDepartment',
+                       'tenantID',
+                       'tenantInstitute',
+                       'tenantBranch',
+                       'desc',
+)
+
+admin.site.register(VerificationInfo,VerificationInfoAdmin)
