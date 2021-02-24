85892d74f79d6461ded20d47e9571353e4908a83
Kush Vora <vorakush@gmail.com>
2021-02-24 22:29:30 +0530

    maha1

--- geo/admin.py
+++ geo/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Maha0, Incidences
+from .models import Maha1, Incidences
 from leaflet.admin import LeafletGeoAdmin
 
 
@@ -10,4 +10,4 @@ class MahaAdmin(LeafletGeoAdmin):
     pass
 
 admin.site.register(Incidences, IncidencesAdmin)
-admin.site.register(Maha0, MahaAdmin)
+admin.site.register(Maha1, MahaAdmin)
--- geo/views.py
+++ geo/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Maha0, Incidences
+from .models import Maha1, Incidences
 from django.views.generic import TemplateView
 from django.core.serializers import serialize
 from django.contrib.gis.geoip2 import GeoIP2
@@ -8,7 +8,7 @@ from .load_layer import run
 
 
 def map_home(request):
-	# run()
+	run()
 	x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
 	if x_forwarded_for:
 		ip = x_forwarded_for.split(',')[0]
@@ -18,14 +18,14 @@ def map_home(request):
 	(lat, lng) = g.lat_lon(ip)
 	# template_name = 'index.html'
 	context = {
-		'lat': lat,
-		'lng': lng,
+		'lat': 6,
+		'lng': 2,
 	}
 	return render(request, 'geo/index.html', context)
 
 
 def maha_datasets(request):
-	names = serialize('geojson', Maha0.objects.all())
+	names = serialize('geojson', Maha1.objects.all())
 	return HttpResponse(names,content_type='json')
 
 def point_datasets(request):
