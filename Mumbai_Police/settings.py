8c41953fa8518a80827449a88553e00bc4effb74
Kush Vora <vorakush@gmail.com>
2021-03-04 00:34:34 +0530

    email confirmation

--- Mumbai_Police/settings.py
+++ Mumbai_Police/settings.py
@@ -217,8 +217,8 @@ MEDIA_URL='/media/'
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = EMAIL_HOST_USER
-EMAIL_HOST_PASSWORD = EMAIL_HOST_PASSWORD
+EMAIL_HOST_USER = 'mumbaipolice366@gmail.com'
+EMAIL_HOST_PASSWORD = 'diveshkushmarmik'
 EMAIL_USE_TLS = True
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- complaints/views.py
+++ complaints/views.py
@@ -5,6 +5,10 @@ from django.http import HttpResponse, HttpResponseRedirect
 from .models import complaintsInfo
 from random import randint
 from secret_keys import *
+from django.core.mail import EmailMessage
+from django.template.loader import render_to_string
+from django.conf import settings
+from complaints import models
 
 @login_required
 def complaints(request):
@@ -20,6 +24,17 @@ def uniqueId():
         uniqueId = uniqueId + str(randint(0,9))
     return uniqueId
 
+def success(request):
+
+    template = render_to_string('home/email_template.html',{'name':request.user.username})
+    email = EmailMessage(
+        'Your Complaint has been filed',
+        template,
+        settings.EMAIL_HOST_USER,
+        [request.user.email],
+    )
+    email.fail_silently=False
+    email.send()
 
 def complaints_form_submission(request):
     fullName = request.POST['fullName']
@@ -39,5 +54,6 @@ def complaints_form_submission(request):
                                  city=city, state=state, country=country, datetime=datetime, crime=crime, desc=desc , ack_no=ack_no)
 
     complaints_info.save()
+    success(request)
     messages.success(request,f'Your Complaint has been filed and your acknowledgment id is {ack_no}.Our team will shortly get in touch with you.')
     return redirect('home')
