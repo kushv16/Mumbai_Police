26f60bf85751def2b24bdfdfa8626252e7b9d16d
Kush Vora <vorakush@gmail.com>
2021-03-25 23:32:59 +0530

    heroku fix

---
+++ secret_keys.py
@@ -0,0 +1,4 @@
+EMAIL_HOST_USER = 'mumbaipolice366@gmail.com'
+EMAIL_HOST_PASSWORD = 'diveshkushmarmik'
+reCAPTCHA_SITE_KEY = '6LePHmQaAAAAAJMMPEWnSEDakG2lep5xJHLoroZk'
+CLOUDINARY_API_SECRET = '0ts3tZxve9J573VJoJV2gQ7wqtA'
